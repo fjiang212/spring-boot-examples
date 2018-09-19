d36d3ede444cecfa0a5b24074e85602dc1fcd99f
Feng Jiang <jiangfeng212@gmail.com>
2018-09-19 00:43:23 -0400

    return source list

--- spring-data-service-es/src/main/java/com/fjiang/springboot/exmaples/es/service/impl/AccountServiceImpl.java
+++ spring-data-service-es/src/main/java/com/fjiang/springboot/exmaples/es/service/impl/AccountServiceImpl.java
@@ -1,11 +1,14 @@
 package com.fjiang.springboot.exmaples.es.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.ResultsExtractor;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
@@ -24,7 +27,19 @@ public class AccountServiceImpl implements AccountService {
 
         NativeSearchQuery build = new NativeSearchQueryBuilder().withIndices("bank").withQuery(query).build();
 
-        return esTemplate.queryForIds(build);
+        return (List<String>) esTemplate.query(build, new ResultsExtractor<List<String>>() {
+
+            @Override
+            public List<String> extract(SearchResponse response) {
+                List<String> results = new ArrayList<String>();
+
+                response.getHits().forEach(hit -> {
+                    results.add(hit.getSourceAsString());
+                });
+                return results;
+            }
+
+        });
     }
 
 }
