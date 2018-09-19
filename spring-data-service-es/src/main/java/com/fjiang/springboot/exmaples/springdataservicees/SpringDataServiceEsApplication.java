72b7035361511d3aaf580004c9886ae5eb7e4a1b
Feng Jiang <jiangfeng212@gmail.com>
2018-09-18 22:23:34 -0400

    Add spring-data-service-es

---
+++ spring-data-service-es/src/main/java/com/fjiang/springboot/exmaples/springdataservicees/SpringDataServiceEsApplication.java
@@ -0,0 +1,12 @@
+package com.fjiang.springboot.exmaples.springdataservicees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataServiceEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataServiceEsApplication.class, args);
+	}
+}
