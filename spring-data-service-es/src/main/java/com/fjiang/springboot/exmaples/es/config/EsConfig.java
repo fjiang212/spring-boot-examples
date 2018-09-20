fe80b30d34fdc9b7c438a9ab2b3312d711b89de8
Feng Jiang <jiangfeng212@gmail.com>
2018-09-19 23:02:48 -0400

    Use resultful client api

--- spring-data-service-es/src/main/java/com/fjiang/springboot/exmaples/es/config/EsConfig.java
+++ spring-data-service-es/src/main/java/com/fjiang/springboot/exmaples/es/config/EsConfig.java
@@ -1,28 +1,34 @@
 package com.fjiang.springboot.exmaples.es.config;
 
-import java.net.InetAddress;
+import java.util.concurrent.TimeUnit;
 
-import org.elasticsearch.client.Client;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class EsConfig {
-    @SuppressWarnings("resource")
-    @Bean
-    public Client client() throws Exception {
-        return new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(
-                new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+    @Bean(destroyMethod = "close")
+    public RestHighLevelClient getRestHighLevelClient() throws Exception {
+        return new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
     }
 
     @Bean
-    public ElasticsearchOperations elasticsearchTemplate() throws Exception {
-        return new ElasticsearchTemplate(client());
+    public SearchSourceBuilder getSearchSourceBuilder() {
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.timeout(new TimeValue(300, TimeUnit.SECONDS));
+
+        return sourceBuilder;
     }
 
+    @Bean
+    public ObjectMapper mapper() {
+        return new ObjectMapper();
+    }
 }
--- spring-data-service-es/src/main/java/com/fjiang/springboot/exmaples/es/service/impl/AccountServiceImpl.java
+++ spring-data-service-es/src/main/java/com/fjiang/springboot/exmaples/es/service/impl/AccountServiceImpl.java
@@ -1,45 +1,89 @@
 package com.fjiang.springboot.exmaples.es.service.impl;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.ResultsExtractor;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fjiang.springboot.exmaples.es.service.AccountService;
 
 @Service
 public class AccountServiceImpl implements AccountService {
     @Autowired
-    private ElasticsearchTemplate esTemplate;
+    private RestHighLevelClient client;
+
+    @Autowired
+    private SearchSourceBuilder sourceBuilder;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public List<String> searchAccount() {
-        QueryBuilder query = QueryBuilders.boolQuery()
-                                          .should(QueryBuilders.queryStringQuery("F").lenient(true).field("gender"));
+        List<String> results = new ArrayList<>();
 
-        NativeSearchQuery build = new NativeSearchQueryBuilder().withIndices("bank").withQuery(query).build();
+        // get indices
+        List<String> indices = getIndices("bank");
+        if (indices.size() > 0) {
 
-        return (List<String>) esTemplate.query(build, new ResultsExtractor<List<String>>() {
+            String[] indexArray = indices.stream().toArray(String[]::new);
 
-            @Override
-            public List<String> extract(SearchResponse response) {
-                List<String> results = new ArrayList<String>();
+            // Build query
+            MatchQueryBuilder builder = new MatchQueryBuilder("gender", "F");
 
-                response.getHits().forEach(hit -> {
+            // Build source
+            sourceBuilder.query(builder);
+
+            String[] includeFields = new String[] { "account_number", "balance", "firstname" };
+            sourceBuilder.fetchSource(includeFields, null);
+
+            // set search request
+            SearchRequest searchRequest = new SearchRequest(indexArray);
+            searchRequest.source(sourceBuilder);
+
+            SearchResponse searchResponse;
+            try {
+                searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+                searchResponse.getHits().forEach(hit -> {
                     results.add(hit.getSourceAsString());
                 });
-                return results;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        });
+        }
+        return results;
     }
 
+    @SuppressWarnings("deprecation")
+    public List<String> getIndices(String name) {
+
+        Response response;
+        try {
+            response = client.getLowLevelClient().performRequest("GET", "/_aliases");
+            String json = EntityUtils.toString(response.getEntity());
+
+            Map<String, Object> aliases = objectMapper.readValue(json, new TypeReference<Map<String, Object>>() {
+            });
+
+            return aliases.keySet().stream().filter(index -> index.contains(name)).collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
